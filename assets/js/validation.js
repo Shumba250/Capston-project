923275c1cf2260df09009b3f471a9d3ad33225a8
Shumba250 <mudakikwaaimable05@gmail.com>
2022-12-13 16:22:07 +0200

    connected validated forms to firebase database

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,3 +1,23 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDOBHqEu_FX5cwR-P79SUHPrl85Eu2R91g",
+	authDomain: "contact-form-e2058.firebaseapp.com",
+	databaseURL: "https://contact-form-e2058-default-rtdb.firebaseio.com",
+	projectId: "contact-form-e2058",
+	storageBucket: "contact-form-e2058.appspot.com",
+	messagingSenderId: "484040147864",
+	appId: "1:484040147864:web:7586cb329bf14a753761ed",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference the database
+var contactFormDB = firebase.database().ref("contactForm");
+var contactFormDBS = firebase.database().ref("loginForm");
+
 function formValidation() {
 	let name = document.getElementById("name").value;
 	let email = document.getElementById("email").value;
@@ -52,4 +72,54 @@ function formValidation() {
 		}, 5000);
 		return false;
 	}
+	saveMessage(name, email, subject, message);
 }
+
+const saveMessage = (name, email, subject, message) => {
+	var newContactForm = contactFormDB.push();
+	newContactForm.set({
+		name: name,
+		email: email,
+		subject: subject,
+		message: message,
+	});
+};
+
+function loginFormValidation() {
+	let idloginFormEmail = document.getElementById("idloginFormEmail").value;
+	let loginFormEmail1 = document.getElementById("loginFormEmail1");
+	let loginFormEmail = document.getElementById("loginFormEmail");
+	let validEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	let Password = document.getElementById("Password").value;
+	let loginFormPassword = document.getElementById("loginFormPassword");
+	if (idloginFormEmail === "") {
+		loginFormEmail1.style.display = "block";
+		setTimeout(() => {
+			loginFormEmail1.style.display = "none";
+		}, 5000);
+		return false;
+	}
+	if (!idloginFormEmail.match(validEmailRegex)) {
+		loginFormEmail.style.display = "block";
+		setTimeout(() => {
+			loginFormEmail.style.display = "none";
+		}, 5000);
+		return false;
+	}
+	if (Password === "") {
+		loginFormPassword.style.display = "block";
+		setTimeout(() => {
+			loginFormPassword.style.display = "none";
+		}, 5000);
+		return false;
+	}
+	saveLoginMessage(idloginFormEmail, Password);
+}
+
+const saveLoginMessage = (idloginFormEmail, Password) => {
+	var newLoginContactForm = contactFormDBS.push();
+	newLoginContactForm.set({
+		email: idloginFormEmail,
+		Password: Password,
+	});
+};
--- dashboard/assets/js/validations.js
+++ dashboard/assets/js/validations.js
@@ -1,8 +1,24 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDOBHqEu_FX5cwR-P79SUHPrl85Eu2R91g",
+	authDomain: "contact-form-e2058.firebaseapp.com",
+	databaseURL: "https://contact-form-e2058-default-rtdb.firebaseio.com",
+	projectId: "contact-form-e2058",
+	storageBucket: "contact-form-e2058.appspot.com",
+	messagingSenderId: "484040147864",
+	appId: "1:484040147864:web:7586cb329bf14a753761ed",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference the database
+var contactFormDB = firebase.database().ref("AddArticle");
+
 function addArticle() {
 	document
 		.getElementById("addArticleButton")
 		.addEventListener("click", function () {
 			let nameValue = document.querySelector("#name").value;
+			let description = document.getElementById("description").value;
 			// console.log(`Title: ${nameValue}`);
 			let descriptionValue = editor.getData();
 			// console.log(`Description: ${descriptionValue}`);
@@ -27,8 +43,28 @@ function addArticle() {
 				}, 5000);
 				return false;
 			}
+			if (description === "") {
+				editorValueError.style.display = "block";
+				setTimeout(() => {
+					editorValueError.style.display = "none";
+				}, 5000);
+			}
+			if (description.length < 20) {
+				editorValueError.style.display = "block";
+				setTimeout(() => {
+					editorValueError.style.display = "none";
+				}, 5000);
+			}
+			saveArticleMessage(nameValue, description);
 		});
 }
+const saveArticleMessage = (nameValue, description) => {
+	var newArticleContactForm = contactFormDB.push();
+	newArticleContactForm.set({
+		title: nameValue,
+		description: description,
+	});
+};
 addArticle();
 
 function hideForm() {
